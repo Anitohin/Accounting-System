7c8b017ea196ef29468776888be11ca97385ab62
Anitohin <anitohinangelodaniel16@gmail.com>
2024-08-20 18:16:56 +0800

    Initial Commit

---
+++ src/main/java/com/group_6/accounting_system/Accounting_System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.group_6.accounting_system;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Accounting_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
