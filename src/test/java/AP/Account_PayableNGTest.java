1e5b6ab021fcc40bbb59f8f64fd3b268045d1570
kaeaa <krishalcaide@gmail.com>
2024-08-23 10:33:10 +0800

    bago

---
+++ src/test/java/AP/Account_PayableNGTest.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package AP;
+
+import static org.testng.Assert.*;
+
+/**
+ *
+ * @author krish
+ */
+public class Account_PayableNGTest {
+    
+    public Account_PayableNGTest() {
+    }
+
+    @org.testng.annotations.BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.testng.annotations.AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.testng.annotations.BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @org.testng.annotations.AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+
+    /**
+     * Test of $$anonymousClasses method, of class Account_Payable.
+     */
+    @org.testng.annotations.Test
+    public void test$$anonymousClasses() {
+        System.out.println("$$anonymousClasses");
+        Account_Payable instance = new Account_Payable();
+        instance.$$anonymousClasses();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
